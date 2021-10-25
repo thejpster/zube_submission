72e1d38af4ca5b3ce21913710ea45dbf18334853
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2021-10-25 13:49:49 +0100

    Fixes to match caravel pin names in mpt.

--- wrapper.v
+++ wrapper.v
@@ -18,12 +18,12 @@ module zube_wrapped_project (
     input wire wb_clk_i,
 `ifdef USE_WB
     input wire wb_rst_i,
-    input wire wb_stb_i,
-    input wire wb_cyc_i,
+    input wire wbs_stb_i,
+    input wire wbs_cyc_i,
     input wire wbs_we_i,
     input wire [3:0] wbs_sel_i,
     input wire [31:0] wbs_dat_i,
-    input wire [31:0] wbs_addr_i,
+    input wire [31:0] wbs_adr_i,
     output wire wbs_ack_o,
     output wire [31:0] wbs_dat_o,
 `endif
@@ -45,7 +45,7 @@ module zube_wrapped_project (
 
     // IRQ
 `ifdef USE_IRQ
-    output wire [2:0] irq,
+    output wire [2:0] user_irq,
 `endif
 
 `ifdef USE_CLK2
@@ -63,7 +63,7 @@ module zube_wrapped_project (
     wire [31:0] buf_la1_data_out;
     wire [`MPRJ_IO_PADS-1:0] buf_io_out;
     wire [`MPRJ_IO_PADS-1:0] buf_io_oeb;
-    wire [2:0] buf_irq;
+    wire [2:0] buf_user_irq;
 
     `ifdef FORMAL
     // formal can't deal with z, so set all outputs to 0 if not active
@@ -79,7 +79,7 @@ module zube_wrapped_project (
     assign io_oeb       = active ? buf_io_oeb       : {`MPRJ_IO_PADS{1'b0}};
     `endif
     `ifdef USE_IRQ
-    assign irq          = active ? buf_irq          : 3'b0;
+    assign user_irq     = active ? buf_user_irq     : 3'b0;
     `endif
     `include "properties.v"
     `else
@@ -97,7 +97,7 @@ module zube_wrapped_project (
     assign io_oeb       = active ? buf_io_oeb       : {`MPRJ_IO_PADS{1'bz}};
     `endif
     `ifdef USE_IRQ
-    assign irq          = active ? buf_irq          : 3'bz;
+    assign user_irq     = active ? buf_user_irq     : 3'bz;
     `endif
     `endif
 
@@ -107,28 +107,28 @@ module zube_wrapped_project (
     assign buf_io_oeb[7:0] = 8'hFF;
     assign buf_io_out[7:0] = 8'h0;
     // Set unused LA bits
-    assign buf_la_data_out[31:0] = 32'h00000000;
+    assign buf_la1_data_out[31:0] = 32'h00000000;
     // Set unused IRQs
-    assign buf_irq[2:1] = 2'b0;
+    assign buf_user_irq[2:1] = 2'b0;
 
     // Instantiate your module here,
     // connecting what you need of the above signals.
     // Use the buffered outputs for your module's outputs.
     zube_wrapper zube_wrapper0(
     .clk(wb_clk_i),
-    .reset_b(la_data_in[0]),
+    .reset_b(la1_data_in[0]),
     // GPIO 37, 36 and 7..0 are used by other things
     .io_in(io_in[35:8]),
     .io_out(buf_io_out[35:8]),
     .io_oeb(buf_io_oeb[35:8]),
-    .wb_cyc_in(wb_cyc_i),
-    .wb_stb_in(wb_stb_i),
+    .wb_cyc_in(wbs_cyc_i),
+    .wb_stb_in(wbs_stb_i),
     .wb_we_in(wbs_we_i),
-    .wb_addr_in(wbs_addr_i),
+    .wb_addr_in(wbs_adr_i),
     .wb_data_in(wbs_dat_i),
     .wb_ack_out(buf_wbs_ack_o),
     .wb_data_out(buf_wbs_dat_o),
-    .irq_out(buf_irq[0])
+    .irq_out(buf_user_irq[0])
 
     );
 
